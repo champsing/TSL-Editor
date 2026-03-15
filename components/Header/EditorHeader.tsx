e70b319da28ae3ac9e7e6f00586e755790790128
champsing <champsingxiang@gmail.com>
2026-03-15 12:44:40 +0800

    remove unused import

--- components/Header/EditorHeader.tsx
+++ components/Header/EditorHeader.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { HelpCircle, Music2, RefreshCcw } from "lucide-react";
+import { HelpCircle, RefreshCcw } from "lucide-react";
 import { VERSION_NUMBER } from "@composables/utils";
 import { JsonButtons } from "./JsonButtons";
 import { HelpModal } from "./HelpModal";
